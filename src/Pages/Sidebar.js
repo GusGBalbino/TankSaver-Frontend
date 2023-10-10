2d02bed1b8573a85740f364aef4a0e00169c5a1d
yasmin <yasmincalixto.yc@gmail.com>
2023-10-10 16:50:34 -0300

    criação do projeto e inicio da sidebar

---
+++ src/Pages/Sidebar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from '../ColorModeSwitcher';
+import { Logo } from '../Logo';
+
+function Sidebar() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid width="15vw" minHeight="100vh" p={3} bgColor={'blue.900'}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Logo h="40vmin" pointerEvents="none" />
+            <Text>
+              Edit <Code fontSize="xl">src/Sidebar.js</Code> and save to reload.
+            </Text>
+            <Link
+              color="teal.500"
+              href="https://chakra-ui.com"
+              fontSize="2xl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn Chakra
+            </Link>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default Sidebar;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Image} from '@chakra-ui/react';
-import logo from './image/logo.svg';
+import logo from '../../image';
 
 export const Logo = props => {
   return <Image  src={logo} {...props} />;
