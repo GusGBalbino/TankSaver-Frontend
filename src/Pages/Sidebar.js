59107f07bd5495f20e12888ae05b6758f0df610a
yasmin <yasmincalixto.yc@gmail.com>
2023-10-18 01:19:17 -0300

    Organizando sidebar e iniciando componentes da sidebar

--- src/Pages/Sidebar.js
+++ src/Pages/Sidebar.js
@@ -7,32 +7,53 @@ import {
   VStack,
   Code,
   Grid,
+  Button,
   theme,
+  Stack,
+  GridItem,
+  Flex
 } from '@chakra-ui/react';
+import { MdBuild, MdCall } from "react-icons/md"
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
 import { Logo } from '../Logo';
+import { OpcoesSidebar } from '../components/OpcoesSidebar/OpcoesSidebar';
+
+import { defineStyle, defineStyleConfig } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
+
+const outline = defineStyle({
+  border: '2px dashed', // change the appearance of the border
+  borderRadius: 0, // remove the border radius
+  fontWeight: 'semibold', // change the font weight
+  colorScheme: 'yellow'
+})
+
+const buttonTheme = defineStyleConfig({
+  variants: { outline },
+})
+
+export const teste = extendTheme({
+  components: { Button: buttonTheme },
+})
+console.log(teste);
 
 function Sidebar() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
-        <Grid width="15vw" minHeight="100vh" p={3} bgColor={'blue.900'}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/Sidebar.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+        <Grid width="15vw" minHeight="100vh" p={7} bgColor={'#131328'}>
+          {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
+          <VStack spacing={5} >
+            <OpcoesSidebar/>
+            <OpcoesSidebar/>
+            <OpcoesSidebar/>
+            <OpcoesSidebar/>
+            <OpcoesSidebar/>
           </VStack>
+          <Flex alignItems={'end'}>
+            <Logo />
+          </Flex>
         </Grid>
       </Box>
     </ChakraProvider>
---
+++ src/components/OpcoesSidebar/OpcoesSidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@chakra-ui/react';
+import { EmailIcon } from '@chakra-ui/icons';
+
+export const OpcoesSidebar = props => {
+    return (
+        <Button leftIcon={<EmailIcon color={'#FFBB0D'} />} variant='outline'  textColor={'white'} borderColor={'#8D7843'} >
+            TESTE
+        </Button>
+    )
+
+}
+
