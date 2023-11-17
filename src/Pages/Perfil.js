412c12eb2b01055c3c18759e2e85c80a470c3d2b
yasmin <yasmincalixto.yc@gmail.com>
2023-11-17 20:14:19 -0300

    Ajuste no perfil

--- src/Pages/Perfil.js
+++ src/Pages/Perfil.js
@@ -16,15 +16,63 @@ import { CardPerfil } from '../components/EditarDados/CardPerfil';
 import { CaixaInfo } from '../components/Informacoes/CaixaInfo';
 
 function Perfil() {
-    function cadastrarUsuario(e) {
-        e.preventDefault();
-        console.log('Cadastrou o usuário!');
-    }
+    
+
+    // useEffect(() => {
+    //     const storedPostoId = localStorage.getItem('postoId');
+    //     const storedPostoName = localStorage.getItem('postoName');
+    //     console.log('Stored Posto ID:', storedPostoId);
+    //     console.log('Stored Posto Name:', storedPostoName);
+    //     if (storedPostoId && storedPostoName) {
+    //         setPostoId(storedPostoId);
+    //         setPostoNome(storedPostoName);
+    //     }
+    // }, []);
+
+    // const cadastrarUsuario = async () => {
+    //     const token = localStorage.getItem('token');
+    //     console.log('Token:', token);
+    //     console.log('Request Data:', {
+    //         nome_fantasia: nome_fantasia,
+    //         bandeira: bandeira,
+    //         cnpj: cnpj,
+    //         email: email,
+    //         endereco: endereco,
+    //         senha: senha,
+    //         posto: postoId
+    //     });
+
+    //     try {
+    //         const response = await axios.get('http://localhost:8000/posto/', {
+    //             nome_fantasia,
+    //             bandeira,
+    //             cnpj,
+    //             email,
+    //             endereco,
+    //             senha,
+    //             posto: postoId
+    //         });
+
+    //         toast({
+    //             position: 'top',
+    //             title: 'Cadastrado com sucesso',
+    //             status: 'success',
+    //             duration: 3000,
+    //             isClosable: true,
+    //         });
+
+    //         onClose();
+    //     } catch (error) {
+    //         console.error('Erro ao adicionar custos:', error);
+    //         console.log('Erro na resposta:', error.response);
+    //     }
+    // };
+
 
     return (
         <ChakraProvider theme={theme}>
             <Rodape />
-            
+
             <Grid
                 display={'flex'}
                 flexDirection={'column'}
@@ -46,26 +94,26 @@ function Perfil() {
 
                         <HStack spacing={4} justifyContent="center" mb={4}>
                             <CaixaInfo title={'Usuário'} info={'Informação que virá do back'} />
-                            <CaixaInfo title={'CNPJ'} info={'Informação que virá do back'}  />
+                            <CaixaInfo title={'CNPJ'} info={'Informação que virá do back'} />
                             <CaixaInfo title={'Telefone Empresarial'} info={'Informação que virá do back'} />
                         </HStack>
 
-                        <CaixaInfo title={'Endereço'} info={'Informação que virá do back'} mb={4}/>
+                        <CaixaInfo title={'Endereço'} info={'Informação que virá do back'} mb={4} />
 
                         <Spacer height={4} />
 
                         <HStack spacing={4} justifyContent="center" mb={4}>
-                            <CaixaInfo title={'CEP'} info={'Informação que virá do back'}/>
+                            <CaixaInfo title={'CEP'} info={'Informação que virá do back'} />
                             <CaixaInfo title={'Município'} info={'Informação que virá do back'} />
                             <CaixaInfo title={'UF'} info={'Informação que virá do back'} />
                         </HStack>
 
-                        <CaixaInfo title={'E-mail'} info={'Informação que virá do back'} mb={4}/>
+                        <CaixaInfo title={'E-mail'} info={'Informação que virá do back'} mb={4} />
 
                         <Spacer height={4} />
 
                         <HStack spacing={4} justifyContent="center" mb={4}>
-                            <CaixaInfo title={'Responsável pela Empresa'} info={'Informação que virá do back'} width="50%"/>
+                            <CaixaInfo title={'Responsável pela Empresa'} info={'Informação que virá do back'} width="50%" />
                             <CaixaInfo title={'Telefone'} info={'Informação que virá do back'} width="50%" />
                         </HStack>
 
